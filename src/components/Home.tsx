fb40554e7b17af6cc8bbd1be48fe5740122f757e
Devadatta Giri <nandanadevadatta161006@gmail.com>
2024-11-24 21:04:35 +0700

    Changing Home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -52,21 +52,210 @@ export default function Home() {
         </div>
       </section>
 
-      {/* Main Section */}
-      <section className="flex justify-center mt-2 mx-5 lg:mx-48">
-        <div className="Animated-Slideup">
-          <div className="w-3/4 mx-auto bg-gray-950 rounded-md p-4 drop-shadow-xl">
-            <h1 className="text-xl lg:text-2xl text-white">Welcome to Abyakta!</h1>
-            <p className="text-white text-sm lg:text-md mt-2 text-justify">
-              We are a team of passionate individuals dedicated to innovation, collaboration, and growth. Whether you're here to learn more about our projects, meet the team, or explore new opportunities, you've come to the right place. Together, we aim to create impactful solutions and foster a community where ideas thrive. Thank you for visiting, and we hope you find inspiration and value in what we share.
-            </p>
-            <h1 className="text-xl lg:text-2xl text-white mt-4">About us.</h1>
-            <p className="text-white text-sm lg:text-md mt-2 text-justify">
-              Part of State University of Surabaya's (Universitas Negeri Surabaya) Informatics Engineering Department. Created for the purpose of developing and implementing innovative projects that benefit the community.
-            </p>
+      {/* Welcoming Section */}
+      <section className="flex flex-col lg:flex-row justify-center items-center">
+        {/* Text Section */}
+        <div className="flex flex-col w-11/12 mt-5 md:w-3/4 lg:w-1/2 text-white mb-8 lg:mb-0 lg:mr-3">
+          <h1 className="text-xl font-bold md:text-3xl lg:text-4xl">
+            Welcome to Our Page!
+          </h1>
+          <p className="text-justify mt-2 text-sm md:text-md">
+            Perkenalan Abyakta Tech itu apa? <br /> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+          </p>
+          <p className="mt-5">Best Regards, Abyakta Teams!</p>
+        </div>
+
+        {/* Team Section */}
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-4 lg:gap-6 lg:mx-10">
+          {/* Team Member Cards */}
+          <div className="flex flex-col items-center text-center">
+            <img
+              className="w-36 h-36 object-cover"
+              src="https://st.depositphotos.com/2101611/3925/v/450/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg"
+              alt="Team Member"
+            />
+            <p className="text-sm text-white mt-2">Foto team <br /> Kalo bisa tiap divisi</p>
+          </div>
+          <div className="flex flex-col items-center text-center">
+            <img
+              className="w-36 h-36 object-cover"
+              src="https://st.depositphotos.com/2101611/3925/v/450/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg"
+              alt="Team Member"
+            />
+            <p className="text-sm text-white mt-2">Foto team <br /> Kalo bisa tiap divisi</p>
+          </div>
+          <div className="flex flex-col items-center text-center">
+            <img
+              className="w-36 h-36 object-cover"
+              src="https://st.depositphotos.com/2101611/3925/v/450/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg"
+              alt="Team Member"
+            />
+            <p className="text-sm text-white mt-2">Foto team <br /> Kalo bisa tiap divisi</p>
+          </div>
+          <div className="flex flex-col items-center text-center">
+            <img
+              className="w-36 h-36 object-cover"
+              src="https://st.depositphotos.com/2101611/3925/v/450/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg"
+              alt="Team Member"
+            />
+            <p className="text-sm text-white mt-2">Foto team <br /> Kalo bisa tiap divisi</p>
+          </div>
+          <div className="flex flex-col items-center text-center">
+            <img
+              className="w-36 h-36 object-cover"
+              src="https://st.depositphotos.com/2101611/3925/v/450/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg"
+              alt="Team Member"
+            />
+            <p className="text-sm text-white mt-2">Foto team <br /> Kalo bisa tiap divisi</p>
+          </div>
+          <div className="flex flex-col items-center text-center">
+            <img
+              className="w-36 h-36 object-cover"
+              src="https://st.depositphotos.com/2101611/3925/v/450/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg"
+              alt="Team Member"
+            />
+            <p className="text-sm text-white mt-2">Foto team <br /> Kalo bisa tiap divisi</p>
           </div>
         </div>
       </section>
+
+      {/* Division Section */}
+      <section className="flex flex-col lg:flex-row justify-center items-center">
+
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-4 lg:gap-6 lg:mx-10">
+        {/* Team Member Cards */}
+        <div className="flex flex-col items-center text-center">
+          <img
+            className="w-36 h-36 object-cover"
+            src="https://st.depositphotos.com/2101611/3925/v/450/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg"
+            alt="Team Member"
+          />
+          <p className="text-sm text-white mt-2">Foto team <br /> Kalo bisa tiap divisi</p>
+        </div>
+        <div className="flex flex-col items-center text-center">
+          <img
+            className="w-36 h-36 object-cover"
+            src="https://st.depositphotos.com/2101611/3925/v/450/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg"
+            alt="Team Member"
+          />
+          <p className="text-sm text-white mt-2">Foto team <br /> Kalo bisa tiap divisi</p>
+        </div>
+        <div className="flex flex-col items-center text-center">
+          <img
+            className="w-36 h-36 object-cover"
+            src="https://st.depositphotos.com/2101611/3925/v/450/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg"
+            alt="Team Member"
+          />
+          <p className="text-sm text-white mt-2">Foto team <br /> Kalo bisa tiap divisi</p>
+        </div>
+        <div className="flex flex-col items-center text-center">
+          <img
+            className="w-36 h-36 object-cover"
+            src="https://st.depositphotos.com/2101611/3925/v/450/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg"
+            alt="Team Member"
+          />
+          <p className="text-sm text-white mt-2">Foto team <br /> Kalo bisa tiap divisi</p>
+        </div>
+        <div className="flex flex-col items-center text-center">
+          <img
+            className="w-36 h-36 object-cover"
+            src="https://st.depositphotos.com/2101611/3925/v/450/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg"
+            alt="Team Member"
+          />
+          <p className="text-sm text-white mt-2">Foto team <br /> Kalo bisa tiap divisi</p>
+        </div>
+        <div className="flex flex-col items-center text-center">
+          <img
+            className="w-36 h-36 object-cover"
+            src="https://st.depositphotos.com/2101611/3925/v/450/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg"
+            alt="Team Member"
+          />
+          <p className="text-sm text-white mt-2">Foto team <br /> Kalo bisa tiap divisi</p>
+        </div>
+      </div>
+
+      <div className="flex flex-col w-11/12 mt-5 md:w-3/4 lg:w-1/2 text-white mb-8 lg:mb-0 lg:mr-3">
+        <h1 className="text-xl font-bold md:text-3xl lg:text-4xl text-end">
+        What Division That We Have?!
+        </h1>
+        <p className="mt-2 text-sm md:text-md text-end">
+          Perkenalan Abyakta Tech itu apa? <br /> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+        </p>
+        <p className="mt-5 text-end">Abyakta Team</p>
+      </div>
+
+    
+      </section>
+
+      {/* Core Team Section */}
+      <section className="flex flex-col justify-center items-center mt-10">
+      
+      <div className="flex flex-col w-11/12 mt-5 md:w-3/4 lg:w-1/2 text-white mb-8 lg:mb-0 lg:mr-3">
+        <h1 className="text-xl font-bold md:text-3xl lg:text-4xl text-center">
+          Meet Our Core Team!
+        </h1>
+        <p className="text-center">Abyakta Team</p>
+      </div>
+
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-4 lg:gap-6 lg:mx-10 mt-8">
+        {/* Team Member Cards */}
+        <div className="flex flex-col items-center text-center">
+          <img
+            className="w-36 h-36 object-cover"
+            src="https://st.depositphotos.com/2101611/3925/v/450/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg"
+            alt="Team Member"
+          />
+          <p className="text-sm text-white mt-2">Foto team <br /> Kalo bisa tiap divisi</p>
+        </div>
+        <div className="flex flex-col items-center text-center">
+          <img
+            className="w-36 h-36 object-cover"
+            src="https://st.depositphotos.com/2101611/3925/v/450/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg"
+            alt="Team Member"
+          />
+          <p className="text-sm text-white mt-2">Foto team <br /> Kalo bisa tiap divisi</p>
+        </div>
+        <div className="flex flex-col items-center text-center">
+          <img
+            className="w-36 h-36 object-cover"
+            src="https://st.depositphotos.com/2101611/3925/v/450/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg"
+            alt="Team Member"
+          />
+          <p className="text-sm text-white mt-2">Foto team <br /> Kalo bisa tiap divisi</p>
+        </div>
+        <div className="flex flex-col items-center text-center">
+          <img
+            className="w-36 h-36 object-cover"
+            src="https://st.depositphotos.com/2101611/3925/v/450/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg"
+            alt="Team Member"
+          />
+          <p className="text-sm text-white mt-2">Foto team <br /> Kalo bisa tiap divisi</p>
+        </div>
+        <div className="flex flex-col items-center text-center">
+          <img
+            className="w-36 h-36 object-cover"
+            src="https://st.depositphotos.com/2101611/3925/v/450/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg"
+            alt="Team Member"
+          />
+          <p className="text-sm text-white mt-2">Foto team <br /> Kalo bisa tiap divisi</p>
+        </div>
+        <div className="flex flex-col items-center text-center">
+          <img
+            className="w-36 h-36 object-cover"
+            src="https://st.depositphotos.com/2101611/3925/v/450/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg"
+            alt="Team Member"
+          />
+          <p className="text-sm text-white mt-2">Foto team <br /> Kalo bisa tiap divisi</p>
+        </div>
+      </div>
+
+      </section>
+
+      {/* Footer Section */}
+      <section className="flex justify-center mt-48 pb-48">
+        <h1 className="text-4xl font-bold text-white">Let’s work together with Us!</h1>
+      </section>
+
     </main>
   );
 }
