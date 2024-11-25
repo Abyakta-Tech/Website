94ab04872b2d4590ab15f5192f20363a02679556
arpthef <harimato9@gmail.com>
2024-11-25 16:53:46 +0700

    fix home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,15 +1,6 @@
 import Headerimage from "../assets/Carousel2.png";
 import { handleScrollToWelcomeSection } from "./HandleHistory";
 
-const TeamSectionImages = [
-  { image: "https://st.depositphotos.com/2101611/3925/v/450/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg", alt: "Team Member" },
-  { image: "https://st.depositphotos.com/2101611/3925/v/450/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg", alt: "Team Member" },
-  { image: "https://st.depositphotos.com/2101611/3925/v/450/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg", alt: "Team Member" },
-  { image: "https://st.depositphotos.com/2101611/3925/v/450/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg", alt: "Team Member" },
-  { image: "https://st.depositphotos.com/2101611/3925/v/450/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg", alt: "Team Member" },
-  { image: "https://st.depositphotos.com/2101611/3925/v/450/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg", alt: "Team Member" },
-];
-
 const DivisionSectionImages = [
   { image: "https://st.depositphotos.com/2101611/3925/v/450/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg", alt: "Team Member" },
   { image: "https://st.depositphotos.com/2101611/3925/v/450/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg", alt: "Team Member" },
@@ -68,32 +59,8 @@ export default function Home() {
           </button>
         </div>
       </section>
-      {/* Welcoming Section */}
-      <section className="flex flex-col lg:flex-row justify-center items-center pt-5" id="welcomesection">
-        <div className="flex flex-col w-11/12 mt-5 md:w-3/4 lg:w-1/2 text-white mb-8 lg:mb-0 lg:mr-3">
-          <h1 className="text-xl font-bold md:text-3xl lg:text-4xl">
-            Welcome to Our Page!
-          </h1>
-          <p className="text-justify mt-2 text-sm md:text-md">
-            Perkenalan Abyakta Tech itu apa? <br /> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-          </p>
-          <p className="mt-5">Best Regards, Abyakta Teams!</p>
-        </div>
-        {/* Team Section */}
-        <div className="grid grid-cols-2 md:grid-cols-3 gap-4 lg:gap-6 lg:mx-10 pt-10 lg:pt-0">
-          {TeamSectionImages.map((item,) => (
-            <div className="flex flex-col items-center text-center">
-              <img
-                className="w-24 h-24 lg:w-36 lg:h-36 object-cover"
-                src={item.image}
-                alt={item.alt}
-              />
-            </div>
-          ))}
-        </div>
-      </section>
       {/* Division Section */}
-      <section className="flex flex-col pt-20 lg:flex-row justify-center items-center">
+      <section className="flex flex-col pt-20 lg:flex-row justify-center items-center" id="welcomesection">
         <div className="grid grid-cols-2 md:grid-cols-3 gap-4 lg:gap-6 lg:mx-10 order-last lg:order-none">
           {DivisionSectionImages.map((item,) => (
             <div className="flex flex-col items-center text-center">
@@ -109,7 +76,7 @@ export default function Home() {
           <h1 className="text-xl font-bold md:text-3xl lg:text-4xl text-center lg:text-end">
             What Division That We Have?!
           </h1>
-          <p className="mt-5 text-center lg:text-end">Abyakta Team</p>
+          <p className="text-center lg:text-end">Abyakta Team</p>
         </div>
       </section>
       {/* Core Team Section */}
